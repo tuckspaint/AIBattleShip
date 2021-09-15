6761db062a3bff62e5521c4b52939726feea1ec4
Eduardo Sanchez <eduardo.sanchez@ku.edu>
2021-09-14 22:04:42 -0500

    new commit

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 #include "Executive.h"
 
 int main() {
-  std::cout << "hello world\n\n";
+  std::cout << "Welcome to BattleShip:\n\n";
   Executive exe;
   exe.run();
 
-
+/*
+Things to clean up:
+- when placing ships 2-6. Player is able to place ships out of board coordinates. board.cpp ships() 2-6
+- Play the game with all 6 ships, test to see if the hitmiss board is registering marked areas.  
+- when ship >= 2: infinite loop menu print after a player hits a ship >= ship2().(2a, 2b->right).
+*/
   return(0);
 };
