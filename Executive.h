49fb69ba2d0902e527272c7e96d6db38d4ea1c57
Eduardo Sanchez <eduardo.sanchez@ku.edu>
2021-09-16 19:30:26 -0500

    Second commit Pushing first round of fixes to the code

--- Executive.h
+++ Executive.h
@@ -1,4 +1,4 @@
-//Executive.h
+//Executive.h iteration 1.1
 #ifndef EXECUTIVE_H
 #define EXECUTIVE_H
 
@@ -6,18 +6,18 @@ class Executive {
     private:
     //initiate current player to 1, player 1 always place's ships first and always shoot first.
     int currentPlayer = 1;
-    
+
     public:
     Executive(){};
     void run();
-    
+
     //prints choice menu for player.
     void printMenu();
-    
-    // dynamic winning conditions based on the number of ships in play. each player have their own hit counts which will compare to this value. 
+
+    // dynamic winning conditions based on the number of ships in play. each player have their own hit counts which will compare to this value.
     // If hitcount of that player matches this number, player wins.
     int winningCondition(int numships);
-    
+
     // at the end of choice 2(choosing which space to attack), game switches players.
     void playerChange();
 
