a674458ddca6a6bf1d800aa288762484480ff484
Eduardo Sanchez <eduardo.sanchez@ku.edu>
2021-09-16 19:10:15 -0500

    Second commit Pushing first round of fixes to the code.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//main.cpp
+//main.cpp iteration 1.1
 #include <iostream>
 #include "Executive.h"
 
@@ -9,9 +9,11 @@ int main() {
 
 /*
 Things to clean up:
-- when placing ships 2-6. Player is able to place ships out of board coordinates. board.cpp ships() 2-6
-- Play the game with all 6 ships, test to see if the hitmiss board is registering marked areas.  
+- when the player is prompt for (int) coordinate (either player 1 or 2). The program runs an infinite loop of asking int coordinates.
+- when placing ships 2-6. Player is able to place part of the ship out coordinates(for example; when placing ship 3, (2,j-right) will register as good coordinates). board.cpp? ships() 2-6
+- Play the game with all 6 ships, test to see if the hitmiss board is registering marked areas.
 - when ship >= 2: infinite loop menu print after a player hits a ship >= ship2().(2a, 2b->right).
+
 */
   return(0);
 };
