c8c7b245ba5f02dfdcdacdaff50a90e7f934a764
Tucker <90000281+sizzub@users.noreply.github.com>
2021-10-10 17:13:22 -0500

    final documentations

--- Executive.h
+++ Executive.h
@@ -51,20 +51,85 @@ class Executive {
     */
     void playerChange();
     
+      /*
+    * @pre none
+    * @post Prints the board[9][10] handed to the function to the consol, then waits for milliSec
+    * @param board[9][10], milliSec
+    * @return none
+    */
     void Printframe(char board[9][10], int milliSec); 
+      /*
+    * @pre none
+    * @post calls Printframe for each frame in the animation and passes it the msPerFrame for each frame
+    * @param msPerFrame
+    * @return none
+    */
     void Playmiss(int msPerFrame);
+    /*
+    * @pre none
+    * @post calls Printframe for each frame in the animation and passes it the msPerFrame for each frame
+    * @param msPerFrame
+    * @return none
+    */
     void Playhit(int msPerFrame);
-    void Playp1win(int msPerFrame, int loops); 
+    /*
+    * @pre none
+    * @post calls Printframe for each frame in the animation and passes it the msPerFrame for each frame, it will repeat the animation for loops number of times
+    * @param msPerFrame, loops
+    * @return none
+    */
+    void Playp1win(int msPerFrame, int loops);
+    /*
+    * @pre none
+    * @post calls Printframe for each frame in the animation and passes it the msPerFrame for each frame, it will repeat the animation for loops number of times
+    * @param msPerFrame, loops
+    * @return none
+    */
     void Playp2win(int msPerFrame, int loops); 
     
+    /*
+    * @pre hitmiss_2 is initialized
+    * @post tries to attack the first valid(not already hit or out of bounds) spot up from initHit, changes aiRow and aiCol to the coordinates of that spot
+    * @param aiRow and aiCol (the coordinates of the valid attack), initHit (the coordinates of the first spot hit), hitmiss_2 (the array of all hits and misses on the board)
+    * @return true if there was a valid spot up from the initHit, false if there was not
+    */
     bool attackUp(int &aiRow, int &aiCol, int initHit[2], char hitmiss_2[9][10]);
+    /*
+    * @pre hitmiss_2 is initialized
+    * @post tries to attack the first valid(not already hit or out of bounds) spot right from initHit, changes aiRow and aiCol to the coordinates of that spot
+    * @param aiRow and aiCol (the coordinates of the valid attack), initHit (the coordinates of the first spot hit), hitmiss_2 (the array of all hits and misses on the board)
+    * @return true if there was a valid spot right from the initHit, false if there was not
+    */
     bool attackRight(int &aiRow, int &aiCol, int initHit[2], char hitmiss_2[9][10]);
+    /*
+    * @pre hitmiss_2 is initialized
+    * @post tries to attack the first valid(not already hit or out of bounds) spot down from initHit, changes aiRow and aiCol to the coordinates of that spot
+    * @param aiRow and aiCol (the coordinates of the valid attack), initHit (the coordinates of the first spot hit), hitmiss_2 (the array of all hits and misses on the board)
+    * @return true if there was a valid spot down from the initHit, false if there was not
+    */
     bool attackDown(int &aiRow, int &aiCol, int initHit[2], char hitmiss_2[9][10]);
+    /*
+    * @pre hitmiss_2 is initialized
+    * @post tries to attack the first valid(not already hit or out of bounds) spot left from initHit, changes aiRow and aiCol to the coordinates of that spot
+    * @param aiRow and aiCol (the coordinates of the valid attack), initHit (the coordinates of the first spot hit), hitmiss_2 (the array of all hits and misses on the board)
+    * @return true if there was a valid spot left from the initHit, false if there was not
+    */
     bool attackLeft(int &aiRow, int &aiCol, int initHit[2], char hitmiss_2[9][10]);
     
-    ///////////////////////////////////New Project 2 - Jason /////////////////////
+    /*
+    * @pre none
+    * @post A random number generator for choosing the AI's row
+    * @param none
+    * @return A number 1-9 for each row
+    */
     int aiRandomRow();
-
+    
+    /*
+    * @pre none
+    * @post A random number generator for choosing the AI's col
+    * @param none
+    * @return A number 1-10 for each column
+    */
     int aiRandomCol();
 
 };
